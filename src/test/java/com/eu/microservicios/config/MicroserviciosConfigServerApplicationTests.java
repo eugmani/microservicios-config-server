35db60564c79c9556bf99e06c263f88ca3755e6c
eugmani <eugmani@gmail.com>
2021-06-29 18:43:45 +0200

    first commit

---
+++ src/test/java/com/eu/microservicios/config/MicroserviciosConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eu.microservicios.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
